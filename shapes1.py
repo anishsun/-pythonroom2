28ed729c02caff54d05cdcc8038d9157377eaa33
Anish5335 <anishgsundar@gmail.com>
2014-05-04 12:11:46 -0700

    pythonroom update

--- shapes1.py
+++ shapes1.py
@@ -1,1 +1,13 @@
-# author: Anish5335
+import turtle
+
+anish = turtle.Turtle()
+
+sides = 8
+length = 40
+angle = 180 -180 * ( sides - 2 ) / sides
+
+numbers = range(0, sides)
+
+for numbers in numbers:
+	anish.forward(length)
+	anish.left(angle)
